37d9a01df1a776cdf846837e03a09a8528bac407
thesct22 <sharathct22@gmail.com>
2021-02-08 22:37:39 +0530

    Removed unnecessary toasts and commented codes

--- app/src/main/java/com/thesct22/envmon/MainActivity.java
+++ app/src/main/java/com/thesct22/envmon/MainActivity.java
@@ -40,35 +40,12 @@ public class MainActivity extends AppCompatActivity {
         maintemp1 = findViewById(R.id.maintemp1);
         maintemp1.setTouchEnabled(true);
         maintemp1.setPinchZoom(true);
-//        LineDataSet temp1lds= new LineDataSet(temp1data, "temp1");
-//        ArrayList<ILineDataSet> temp1ilds = new ArrayList<>();
-//        temp1ilds.add(temp1lds);
+
 
         mdb = FirebaseDatabase.getInstance().getReference().child("temp").child("temp1");
 
         retrievedata();
-//        LineData lineData = new LineData(temp1ilds);
-//        maintemp1.setData(lineData);
-//        maintemp1.invalidate();
-        //if you want set background color use below method
-        //lineChart.setBackgroundColor(Color.RED);
 
-        // set text if data are are not available
-        //maintemp1.setNoDataText("Data not Available");
-
-        //you can modify your line chart graph according to your requirement there are lots of method available in this library
-
-        //now customize line chart
-
-//        temp1lds.setColor(Color.BLUE);
-//        temp1lds.setCircleColor(Color.GREEN);
-//        temp1lds.setDrawCircles(true);
-//        temp1lds.setDrawCircleHole(true);
-//        temp1lds.setLineWidth(5);
-//        temp1lds.setCircleRadius(10);
-//        temp1lds.setCircleHoleRadius(10);
-//        temp1lds.setValueTextSize(10);
-//        temp1lds.setValueTextColor(Color.BLACK);
 
     }
 
@@ -82,16 +59,12 @@ public class MainActivity extends AppCompatActivity {
                 if(dataSnapshot.hasChildren()){
 
                     for(DataSnapshot mydss:dataSnapshot.getChildren()){
-                        //tempclass tempclass1=mydss.getValue(tempclass.class);
-                        //Context context = getApplicationContext();
+
                         String tsstr = mydss.getKey();
                         long ts=Long.parseLong(tsstr);
                         Long templong=mydss.getValue(Long.class);
                         int tempint=templong.intValue();
-                        //int duration = Toast.LENGTH_LONG;
 
-                        //Toast toast = Toast.makeText(context, meh.toString(), duration);
-                        //toast.show();
                         datavals.add(new Entry((int)ts,tempint));
                     }
                     showchart(datavals);
@@ -105,28 +78,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Context context = getApplicationContext();
-                CharSequence text = "hello";
+                CharSequence text = "Database Loading error";
                 int duration = Toast.LENGTH_LONG;
 
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
-                // Failed to read value
-                //Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
 
     }
     private void showchart(ArrayList<Entry> showvals){
-
-        for(Entry ee: showvals){
-            double qwe=ee.getX();
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_LONG;
-
-            Toast toast = Toast.makeText(context, String.valueOf(qwe), duration);
-            toast.show();
-
-        }
 
         temp1lds.setValues(showvals);
         temp1lds.setLabel("temp1");
@@ -137,11 +98,9 @@ public class MainActivity extends AppCompatActivity {
         maintemp1.setData(temp1ld);
         maintemp1.invalidate();
 
-
-        //maintemp1.setNoDataText("Data not Available");
+        maintemp1.setNoDataText("Data not Available");
 
         //you can modify your line chart graph according to your requirement there are lots of method available in this library
-
         //now customize line chart
 
         temp1lds.setColor(Color.BLUE);
