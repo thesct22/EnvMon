e3d6f22632c3ad6e3bdb157ad5ea6f918f7e0bcf
Alopoyon <achint1820@gmail.com>
2021-03-12 12:35:31 +0530

    Authentication

--- app/src/main/java/com/thesct22/envmon/Login.java
+++ app/src/main/java/com/thesct22/envmon/Login.java
@@ -1,20 +1,108 @@
 package com.thesct22.envmon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
-    EditText txtLUsername;
-    EditText txtLPassword;
-    Button bLogin;
+    private EditText txtLEmail,txtLPassword;
+    private Button bLogin;
+    private ProgressBar progressBarL;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth= FirebaseAuth.getInstance();
+        txtLEmail=findViewById(R.id.etLEmail);
+        txtLPassword=findViewById(R.id.etLPassword);
+        bLogin=findViewById(R.id.btnLogin);
+        progressBarL=findViewById(R.id.progressBar);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mAuth.getCurrentUser();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(Login.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(Login.this, MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(Login.this,"Please Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtLEmail.getText().toString();
+                String pwd = txtLPassword.getText().toString();
+                if(email.isEmpty()){
+                    txtLEmail.setError("Please enter email id");
+                    txtLEmail.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    txtLPassword.setError("Please enter your password");
+                    txtLPassword.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(Login.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Login.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent intToHome = new Intent(Login.this,MainActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(Login.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+//        Register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intSignUp = new Intent(Login.this, MainActivity.class);
+//                startActivity(intSignUp);
+//            }
+//        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
 }
--- app/src/main/java/com/thesct22/envmon/Register.java
+++ app/src/main/java/com/thesct22/envmon/Register.java
@@ -1,22 +1,87 @@
 package com.thesct22.envmon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
-    EditText txtRUsername;
-    EditText txtREmail;
-    EditText txtRPassword;
-    EditText txtRPassword2;
-    Button bRegister;
+    private EditText txtRUsername,txtREmail,txtRPassword;
+    //private EditText txtRPassword2;
+    private Button bRegister;
+    private ProgressBar progressBarR;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth=FirebaseAuth.getInstance();
+        txtRUsername=findViewById(R.id.etUsername);
+        txtREmail=findViewById(R.id.etEmail);
+        txtRPassword=findViewById(R.id.etRPassword);
+        //txtRPassword2=findViewById(R.id.etRPassword2);
+        bRegister=findViewById(R.id.btnRegister);
+        progressBarR=findViewById(R.id.progressBar2);
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = txtREmail.getText().toString();
+                String pwd = txtRPassword.getText().toString();
+                if(email.isEmpty()){
+                    txtREmail.setError("Please enter email id");
+                    txtREmail.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    txtRPassword.setError("Please enter your password");
+                    txtRPassword.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(Register.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Register.this,"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(Register.this,MainActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(Register.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+//        tvSignIn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(Register.this,Login.class);
+//                startActivity(i);
+//            }
+//        });
     }
 }
